02c763e109b86cc5a5ecfcd3490c4784e50988a7
jaronimoe <johannessorger@gmx.net>
2015-11-30 17:07:17 +0100

    added Protein Settings Exporter for handling export/import of cutaway protein settings

---
+++ Assets/Scripts/tmp_util.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml.Serialization;
+using System.IO;
+using System.Collections.Generic;
+
+public class tmp_util : MonoBehaviour {
+
+    private XmlSerializer serializer;
+    public string path = "C:\\proteins.xml";
+    private FileStream stream;
+
+	// Use this for initialization
+	void Start ()
+    {
+        ////read
+        //serializer = new XmlSerializer(typeof(CutParametersContainer));
+        //stream = new FileStream(path, FileMode.Open);
+        //var container = serializer.Deserialize(stream) as MonsterContainer;
+        //stream.Close();
+
+
+        ////write
+        // serializer = new XmlSerializer(typeof(CutParametersContainer));
+        // stream = new FileStream(path, FileMode.Create);
+        // serializer.Serialize(stream, this);
+        // stream.Close();
+	}
+	
+
+    public void ExportProteinSettings()
+    {
+        CutParametersContainer exportParams = new CutParametersContainer();
+
+        foreach (CutObject cuto in SceneManager.Instance.CutObjects)
+        {
+            exportParams.ProteinTypeParameters.Add(cuto.ProteinTypeParameters);
+        }
+
+        ////write
+        serializer = new XmlSerializer(typeof(CutParametersContainer));
+        stream = new FileStream(path, FileMode.Create);
+        serializer.Serialize(stream, exportParams);
+        stream.Close();
+    }
+
+    public void ImportProteinSettings()
+    {
+        ////read
+        serializer = new XmlSerializer(typeof(CutParametersContainer));
+        stream = new FileStream(path, FileMode.Open);
+        CutParametersContainer importParams = serializer.Deserialize(stream) as CutParametersContainer;
+        stream.Close();
+
+        for (int i = 0; i < importParams.ProteinTypeParameters.Count && i < SceneManager.Instance.CutObjects.Count; i++)
+        {
+            SceneManager.Instance.CutObjects[i].ProteinTypeParameters = importParams.ProteinTypeParameters[i]; 
+        }
+    }
+
+    [XmlRoot("CutParametersContainer")]
+    public class CutParametersContainer
+    {
+        [XmlArray("List of ParamSets")]
+        [XmlArrayItem("ParamSet")]
+        public List<List<CutParameters>> ProteinTypeParameters = new List<List<CutParameters>>();
+    }
+}
