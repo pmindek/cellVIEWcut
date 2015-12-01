8614d45cdea51f3cd805b33f6e5c1bfade6aefb4
matmuze <matmuze@users.noreply.github.com>
2015-12-01 03:19:38 +0100

    rgergergrvgtrht

--- Assets/Scripts/MyUtility.cs
+++ Assets/Scripts/MyUtility.cs
@@ -690,10 +690,13 @@ public static class MyExtensions
         // This resets the append buffer buffer to 0
         var dummy1 = RenderTexture.GetTemporary(8, 8, 24, RenderTextureFormat.ARGB32);
         var dummy2 = RenderTexture.GetTemporary(8, 8, 24, RenderTextureFormat.ARGB32);
+        var active = RenderTexture.active;
 
         Graphics.SetRandomWriteTarget(1, appendBuffer);
         Graphics.Blit(dummy1, dummy2);
         Graphics.ClearRandomWriteTargets();
+
+        RenderTexture.active = active;
 
         dummy1.Release();
         dummy2.Release();
